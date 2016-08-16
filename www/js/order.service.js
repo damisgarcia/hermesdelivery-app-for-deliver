bad5097898a68a8cd0681582ca68f143093fb14e
damisgarcia <damisgarcia@gmail.com>
2016-08-16 18:41:29 -0300

    Production URL

--- www/js/order.service.js
+++ www/js/order.service.js
@@ -1,7 +1,7 @@
 angular.module('starter').factory("Order", function($http, $Cache){
   return {
     all: function(callback){
-      var baseurl = "http://localhost:3000/api/v1/agents/orders/access_key/" + $Cache.get().auth
+      var baseurl = "http://hermesdelivery.herokuapp.com/api/v1/agents/orders/access_key/" + $Cache.get().auth
       $http.get(baseurl).then(callback)
     }
   }
