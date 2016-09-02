c505a31210b159706b1826bc794631172529e229
Damis Garcia <damisgarcia@gmail.com>
2016-09-02 00:43:20 -0300

    Add Geolocation

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,10 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 angular.module('starter', ['ionic','yaru22.angular-timeago'])
-.config(function($stateProvider, $urlRouterProvider, $httpProvider){
+.config(function($stateProvider, $urlRouterProvider, $httpProvider, $ionicConfigProvider){
+  // View Cache
+  $ionicConfigProvider.views.maxCache(0)
+
   $urlRouterProvider.otherwise('/orders')
 
   $stateProvider
@@ -30,7 +33,7 @@ angular.module('starter', ['ionic','yaru22.angular-timeago'])
 
   $httpProvider.interceptors.push('DefaultHttpInteceptor')
 })
-.run(function($ionicPlatform) {
+.run(function($ionicPlatform, $http) {
   $ionicPlatform.ready(function() {
     if(window.cordova && window.cordova.plugins.Keyboard) {
       // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
@@ -45,7 +48,64 @@ angular.module('starter', ['ionic','yaru22.angular-timeago'])
     if(window.StatusBar) {
       StatusBar.styleDefault();
     }
-    // var bgGeo = window.BackgroundGeolocation
-    // console.log(bgGeo)
+
+    var bgGeo = window.BackgroundGeolocation;
+
+    //This callback will be executed every time a geolocation is recorded in the background.
+    var callbackFn = function(location, taskId) {
+        var coords = location.coords;
+        var lat    = coords.latitude;
+        var lng    = coords.longitude;
+        var baseurl = "http://hermesdelivery.herokuapp.com/api/v1/agents/location/access_key/" + $Cache.get().auth
+        // var baseurl = "http://192.168.25.11:3000/api/v1/agents/location/access_key/" + $Cache.get().auth
+        console.log('- Location: ', JSON.stringify(location));
+        $http.post(baseurl, location.coords).success(function(data){
+          console.log("- Storage Location in Hermes:", data)
+        })
+
+        // Must signal completion of your callbackFn.
+        bgGeo.finish(taskId);
+    };
+
+    // This callback will be executed if a location-error occurs.  Eg: this will be called if user disables location-services.
+    var failureFn = function(errorCode) {
+        console.warn('- BackgroundGeoLocation error: ', errorCode);
+    }
+
+    // Listen to location events & errors.
+    bgGeo.on('location', callbackFn, failureFn);
+
+    // Fired whenever state changes from moving->stationary or vice-versa.
+    bgGeo.on('motionchange', function(isMoving) {
+      console.log('- onMotionChange: ', isMoving);
+    });
+
+    // BackgroundGeoLocation is highly configurable.
+    bgGeo.configure({
+        // Geolocation config
+        desiredAccuracy: 0,
+        distanceFilter: 10,
+        stationaryRadius: 50,
+        locationUpdateInterval: 1000,
+        fastestLocationUpdateInterval: 5000,
+
+        // Activity Recognition config
+        activityType: 'AutomotiveNavigation',
+        activityRecognitionInterval: 5000,
+        stopTimeout: 5,
+
+        // Application config
+        debug: true,
+        stopOnTerminate: false,
+        startOnBoot: true,
+    }, function(state) {
+        // This callback is executed when the plugin is ready to use.
+        console.log('BackgroundGeolocation ready: ', state);
+        if (!state.enabled) {
+            bgGeo.start();
+        }
+    });
+
+    bgGeo.start()
   });
 })
--- www/js/order.service.js
+++ www/js/order.service.js
@@ -2,6 +2,7 @@ angular.module('starter').factory("Order", ['$http', '$Cache',function($http, $C
   return {
     all: function(callback, error){
       var baseurl = "http://hermesdelivery.herokuapp.com/api/v1/agents/orders/access_key/" + $Cache.get().auth
+      // var baseurl = "http://192.168.25.11:3000/api/v1/agents/orders/access_key/" + $Cache.get().auth
       $http.get(baseurl).then(callback)
     }
   }
